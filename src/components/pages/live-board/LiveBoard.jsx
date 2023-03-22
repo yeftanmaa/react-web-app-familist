a16f20fed87cc3ac6a19d53819e4c76224d754d5
Nathan Johanes Yefta <102526580+yeftanmaa@users.noreply.github.com>
2023-03-22 11:53:16 +0700

    DEV-038: Fixed Tab Issue & Added Loading Skeleton
    Tab issue is fixed but not really well since this caused by scrollbar appearing;
    Added skeleton so user know when its loading data;

--- src/components/pages/live-board/LiveBoard.jsx
+++ src/components/pages/live-board/LiveBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
-import { Typography, Avatar, InputLabel, Select, MenuItem } from "@mui/material";
+import { Typography, Avatar, InputLabel, Select, MenuItem, Skeleton } from "@mui/material";
 import { Box, Container } from "@mui/system";
 import { collection, query, getDocs, orderBy, limit } from "firebase/firestore";
 import { db } from "../../../config/firebase";
@@ -29,11 +29,14 @@ function App() {
   };
 
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const currentMonth = new Date().toLocaleDateString().substring(0, 1);
   const currentYear = new Date().getFullYear();
 
   useEffect(() => {
       const fetchData = async () => {
+          setIsLoading(true);
           const schedulerCol = collection(db, 'scheduler');
           const schedulerQuery = query(schedulerCol);
           const schedulerSnapshot = await getDocs(schedulerQuery);
@@ -50,6 +53,7 @@ function App() {
           }
       
           setData(docs);
+          setIsLoading(false);
       };
 
       fetchData();
@@ -78,6 +82,12 @@ function App() {
   }, [data, currentMonth])
   
   const [columns, setColumns] = useState({});
+
+  // Duplicate skeleton
+  const skeletonArray = [];
+  for (let i = 0; i < 7; i++) {
+    skeletonArray.push((<Skeleton variant="rounded" height={200} sx={{marginBottom: '15px'}} />))
+  }
 
   return (
     <div>
@@ -145,92 +155,98 @@ function App() {
                               borderRadius: 6
                             }}
                           >
-                            {/* we put every cards inside the columns */}
-                            {column.items.map((item, index) => {
-                              return (
-                                // then we make each cards is draggable
-                                <Draggable>
-                                  {(provided, snapshot) => {
-                                    return (
-                                      <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{
-                                          userSelect: "none",
-                                          padding: 15,
-                                          borderRadius: '7px',
-                                          margin: "0 0 15px 0",
-                                          minHeight: "120px",
-                                          backgroundColor: snapshot.isDragging
-                                            ? "white"
-                                            : "white",
-                                          color: "black",
-                                          boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.1)',
-                                          position: 'relative',
-                                          ...provided.draggableProps.style
-                                        }}
-                                      >
-                                        <Box position={"absolute"} top={13} right={11}>
-                                          <Box>
-                                            {
-                                              item.isCicilan ? (
-                                                <Box sx={{backgroundColor: '#aa16db', padding: '3px 10px', borderRadius: '20px'}}>
-                                                  <Typography variant="caption" color={"white"}>Cicilan</Typography>
-                                                </Box>
-                                              ) : (
-                                                <Box sx={{backgroundColor: 'rgba(36, 79, 255, 0.8)', padding: '3px 10px', borderRadius: '20px'}}>
-                                                  <Typography variant="caption" color={"white"}>Tagihan Reguler</Typography>
-                                                </Box>
-                                            )}
+                          {isLoading ? (
+                             skeletonArray
+                          ) : (
+                            <>
+                              {/* we put every cards inside the columns */}
+                              {column.items.map((item, index) => {
+                                return (
+                                  // then we make each cards is draggable
+                                  <Draggable>
+                                    {(provided, snapshot) => {
+                                      return (
+                                        <div
+                                          ref={provided.innerRef}
+                                          {...provided.draggableProps}
+                                          {...provided.dragHandleProps}
+                                          style={{
+                                            userSelect: "none",
+                                            padding: 15,
+                                            borderRadius: '7px',
+                                            margin: "0 0 15px 0",
+                                            minHeight: "120px",
+                                            backgroundColor: snapshot.isDragging
+                                              ? "white"
+                                              : "white",
+                                            color: "black",
+                                            boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.1)',
+                                            position: 'relative',
+                                            ...provided.draggableProps.style
+                                          }}
+                                        >
+                                          <Box position={"absolute"} top={13} right={11}>
+                                            <Box>
+                                              {
+                                                item.isCicilan ? (
+                                                  <Box sx={{backgroundColor: '#aa16db', padding: '3px 10px', borderRadius: '20px'}}>
+                                                    <Typography variant="caption" color={"white"}>Cicilan</Typography>
+                                                  </Box>
+                                                ) : (
+                                                  <Box sx={{backgroundColor: 'rgba(36, 79, 255, 0.8)', padding: '3px 10px', borderRadius: '20px'}}>
+                                                    <Typography variant="caption" color={"white"}>Tagihan Reguler</Typography>
+                                                  </Box>
+                                              )}
+                                            </Box>
                                           </Box>
-                                        </Box>
+                                          
+                                          <Typography>{item.title}</Typography>
+
+                                          <Box sx={{maxWidth: '260px', marginTop: 1}}>
+                                            <Typography sx={{ fontSize: 13, color: 'black'}}>
+                                              {
+                                                (columnId === 'nextpayment') ? '' :
+                                                (columnId === 'notpaid') ? 'Tenggat waktu: ' + item.deadline.substring(0, 2) + '/' + currentMonth + '/2023' :
+                                                (item.isCicilan) ? 'Sisa pembayaran: ' + item.payment?.remainingBill?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })
+                                                : ''
+                                              }
+                                            </Typography>
+
+                                            <Typography fontStyle={'italic'} sx={{ fontSize: 13, color: '#C9C9C9'}}>
+                                              {
+                                                (columnId === 'nextpayment') ? "Next Payment: " + getNextMonthName() + ' ' + currentYear :
+                                                (columnId === 'notpaid') ? 'Terakhir dibayar: ' + item.payment.lastPaid.toDate().toLocaleDateString() :
+                                                (columnId === 'paid') ? 'Paid on: ' + item.payment.lastPaid.toDate().toLocaleDateString()
+                                                : 'undefined'
+                                              }
+                                            </Typography>
+
+                                          </Box>
+
+                                          <Box position={"absolute"} bottom={10} left={15}>
+                                            <Typography variant="h6" sx={{color: '#c4183b', fontWeight: 600, fontSize: '16px'}} >
+                                              {
+                                                (item.fixedBill === undefined && columnId !== 'nextpayment') ? item.payment.amountPaid?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) :
+                                                (columnId === 'nextpayment' && item.fixedBill === undefined) ? (<Typography color={'grey'}>Not measured</Typography>) :
+                                                item.fixedBill?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })
+                                              }
+                                            </Typography>
+                                          </Box>
+
+                                          <Box position={"absolute"} bottom={10} right={10} display={"flex"} alignItems={"center"} gap={1}>
+                                            <Avatar sx={{width: 32, height: 32, fontSize: 15}}>T</Avatar>
+                                          </Box>
+                                          
+                                        </div>
                                         
-                                        <Typography>{item.title}</Typography>
-
-                                        <Box sx={{maxWidth: '260px', marginTop: 1}}>
-                                          <Typography sx={{ fontSize: 13, color: 'black'}}>
-                                            {
-                                              (columnId === 'nextpayment') ? '' :
-                                              (columnId === 'notpaid') ? 'Tenggat waktu: ' + item.deadline.substring(0, 2) + '/' + currentMonth + '/2023' :
-                                              (item.isCicilan) ? 'Sisa pembayaran: ' + item.payment?.remainingBill?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })
-                                              : ''
-                                            }
-                                          </Typography>
-
-                                          <Typography fontStyle={'italic'} sx={{ fontSize: 13, color: '#C9C9C9'}}>
-                                            {
-                                              (columnId === 'nextpayment') ? "Next Payment: " + getNextMonthName() + ' ' + currentYear :
-                                              (columnId === 'notpaid') ? 'Terakhir dibayar: ' + item.payment.lastPaid.toDate().toLocaleDateString() :
-                                              (columnId === 'paid') ? 'Paid on: ' + item.payment.lastPaid.toDate().toLocaleDateString()
-                                              : 'undefined'
-                                            }
-                                          </Typography>
-
-                                        </Box>
-
-                                        <Box position={"absolute"} bottom={10} left={15}>
-                                          <Typography variant="h6" sx={{color: '#c4183b', fontWeight: 600, fontSize: '16px'}} >
-                                            {
-                                              (item.fixedBill === undefined && columnId !== 'nextpayment') ? item.payment.amountPaid?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) :
-                                              (columnId === 'nextpayment' && item.fixedBill === undefined) ? (<Typography color={'grey'}>Not measured</Typography>) :
-                                              item.fixedBill?.toLocaleString('id-ID', { style: 'currency', currency: 'IDR' })
-                                            }
-                                          </Typography>
-                                        </Box>
-
-                                        <Box position={"absolute"} bottom={10} right={10} display={"flex"} alignItems={"center"} gap={1}>
-                                          <Avatar sx={{width: 32, height: 32, fontSize: 15}}>T</Avatar>
-                                        </Box>
-                                        
-                                      </div>
-                                      
-                                    );
-                                  }}
-                                </Draggable>
-                              );
-                            })}
-                            {provided.placeholder}
+                                      );
+                                    }}
+                                  </Draggable>
+                                );
+                              })}
+                              {provided.placeholder}
+                            </>
+                          )}
                           </div>
                         );
                       }}
